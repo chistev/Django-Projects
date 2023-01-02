74c26663bd940acd22f7220549d4d523d7e06e89
chistev <115540580+chistev@users.noreply.github.com>
2023-01-02 10:42:38 +0100

    Add files via upload

--- photoAlbum/photoAlbum/photos/views.py
+++ photoAlbum/photoAlbum/photos/views.py
@@ -1,14 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Photo
 
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+
+    category = request.GET.get('category')
+    if category is None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
+    categories = Category.objects.all()
+    context = {'categories': categories, 'photos': photos}
+    return render(request, 'photos/gallery.html', context)
 
 
 def view_photo(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo':photo})
 
 
 def add_photo(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
 
