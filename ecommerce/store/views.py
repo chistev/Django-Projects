bf39c2c3bb6908a895ac4c56b48b8edbf3b22c1f
chistev <115540580+chistev@users.noreply.github.com>
2023-02-07 14:26:30 +0100

    Add files via upload

---
+++ ecommerce/store/views.py
@@ -0,0 +1,63 @@
+import json
+
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import *
+
+
+def index(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/index.html', context )
+
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+
+    context = {'items': items, 'order': order}
+
+    return render(request, 'store/checkout.html', context)
+
+
+def update_item(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    return JsonResponse('user is authenticated', safe=False)
